0f77b35221178e2f026da60e8fcc1d39c1994af8
Benjamin Anderson <benjaminanderson@Benjamins-MacBook-Air-251.local>
2025-03-21 11:35:10 -0600

    implemented play.jsx

--- src/play/play.jsx
+++ src/play/play.jsx
@@ -1,9 +1,92 @@
 import React from 'react';
+import './play.css';
 
 export function Play() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>Play displayed here</div>
-    </main>
+    <main className="bg-secondary">
+            <div className="players">
+                Player
+                <span className="player-name">Jamin</span>
+            
+                <div className="event"><span className="player-event">Linus</span> scored 377</div>
+                <div className="event"><span className="player-event">Ada</span> started a new game</div>
+                <div className="event"><span className="player-event">Ada</span> stated a new game</div>
+                <div className="event"><span className="player-event">Tim</span> wom against John</div>
+                <div className="event"><span className="system-event">Game</span> connected</div>
+            </div>    
+
+            <div className="game"> 
+                <div>
+                    <div className="connect-four">
+                        <a>Connect Four<sup>&reg;</sup></a>
+                    </div>
+
+                    <div className="user-score">
+                        <div className="score">--</div>
+                        <button className="btn btn-primary custom-btn">Reset</button>
+                    </div>
+                </div>    
+
+                <div className="grid-container">
+                    <div className="square"></div>
+                    <div className="square"></div>
+                    <div className="square"></div>
+                    <div className="square"></div>
+                    <div className="square"></div>
+                    <div className="square"></div>
+                    <div className="square"></div>
+
+                    <div className="square"></div>
+                    <div className="square"></div>
+                    <div className="square"></div>
+                    <div className="square"></div>
+                    <div className="square"></div>
+                    <div className="square"></div>
+                    <div className="square"></div>
+
+                    <div className="square"></div>
+                    <div className="square"></div>
+                    <div className="square"></div>
+                    <div className="square"></div>
+                    <div className="square"></div>
+                    <div className="square"></div>
+                    <div className="square"></div>
+
+                    <div className="square"></div>
+                    <div className="square"></div>
+                    <div className="square"></div>
+                    <div className="square"></div>
+                    <div className="square"></div>
+                    <div className="square"></div>
+                    <div className="square"></div>
+
+                    <div className="square"></div>
+                    <div className="square"></div>
+                    <div className="square"></div>
+                    <div className="square"></div>
+                    <div className="square"></div>
+                    <div className="square"></div>
+                    <div className="square"></div>
+
+                    <div className="square"></div>
+                    <div className="square"></div>
+                    <div className="square"></div>
+                    <div className="square"></div>
+                    <div className="square"></div>
+                    <div className="square"></div>
+                    <div className="square"></div>
+                </div>
+
+                <div className="button-container">
+                    <td><button className="button"></button></td>
+                    <td><button className="button"></button></td>
+                    <td><button className="button"></button></td>
+                    <td><button className="button"></button></td>
+                    <td><button className="button"></button></td>
+                    <td><button className="button"></button></td>
+                    <td><button className="button"></button></td>
+                </div>
+            </div>  
+        </main>
   );
 }
